ea97fe3b0c18f1a1363d5c43bae909f90dcfc39c
Sang Soo Ra <nadang82@gmail.com>
2020-03-12 18:14:11 +0100

    updated style

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,6 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :description, presence: true
   validates :cocktail_id, presence: true
-  validates :ingredient, presence: true
-  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates :ingredient_id, presence: true
+  # validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+ingredient['drinks'].each do |i|
+  Ingredient.create!(name: i['strIngredient1'])
+end
