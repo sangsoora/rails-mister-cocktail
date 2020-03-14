6f0ceef60afc283a8c2e5756767a1ef9723dc684
Sang Soo Ra <nadang82@gmail.com>
2020-03-14 19:19:06 +0100

    finished front & back

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,6 +4,5 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail_id, presence: true
   validates :ingredient_id, presence: true
-  # validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
